b493dd03ec69ab071565230d9a5dcbd8a03fb588
sabarim6369 <sabarim6369@gmail.com>
2025-01-06 18:12:47 +0530

    first commit

--- clothing/src/app/api/gift/route.js
+++ clothing/src/app/api/gift/route.js
@@ -1,20 +1,22 @@
-import connectMongoDB from "../Connection";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
+const GiftSchema = new mongoose.Schema({}, { collection: 'gifts' });
+const Gift = mongoose.models.Gift || mongoose.model('Gift', GiftSchema);
+
 export async function GET(request) {
   try {
     await connectMongoDB();
 
-    const gift = await mongoose.connection.db
-      .collection('gifts')
-      .find({})
-      .toArray();
-console.log(gift)
-    return new Response(JSON.stringify(gift), {
+    // Use Mongoose model to fetch data
+    const gifts = await Gift.find({});
+    console.log(gifts);
+
+    return new Response(JSON.stringify(gifts), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    console.error('Error fetching categories:', error);
+    console.error('Error fetching gifts:', error);
 
     return new Response(
       JSON.stringify({ error: 'Internal Server Error' }),
